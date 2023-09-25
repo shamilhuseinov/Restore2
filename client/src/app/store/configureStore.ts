4fbacb71f23a2e41ccba2e4fb4d367463e2e89fa
shamilhuseinov <shamilrh@code.edu.az>
2023-09-25 23:27:12 +0400

    End of section 7

---
+++ client/src/app/store/configureStore.ts
@@ -0,0 +1,24 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "../../features/contact/counterSlice";
+import { type } from "os";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { basketSlice } from "../../features/basket/basketSlice";
+import { catalogSlice } from "../../features/catalog/catalogSlice";
+
+// export function configureStore(){
+//     return createStore(counterReducer)
+// }
+
+export const store = configureStore({
+    reducer: {
+        counter: counterSlice.reducer,
+        basket: basketSlice.reducer,
+        catalog: catalogSlice.reducer
+    }
+})
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AddDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AddDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
